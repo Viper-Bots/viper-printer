df3af4eaaadf96c8df1b0e7a86ebbea62f1dfb09
Josh Renaud <josh@joshrenaud.com>
2021-05-15 14:15:18 -0500

    Change png import command, plus add comments

--- printer.py
+++ printer.py
@@ -1,7 +1,7 @@
 from ev3dev2.motor import *
 import datetime
 import time
-import png
+from purepng import png
 import argparse
 
 SECONDS_1 = datetime.timedelta(seconds=1)
@@ -19,6 +19,7 @@ motor_linefeed = LargeMotor("outB")
 motor_print_head = LargeMotor("outC")
 motor_pen = MediumMotor("outD")
 
+# Carriage return
 def cr():
 	motor_print_head.run_to_abs_pos(position_sp=0, speed_sp=400, stop_action='hold')
 	motor_print_head.wait_while('running',timeout=1000)
@@ -52,7 +53,6 @@ def backward(amt=0):
 	motor_linefeed.wait_while('running',timeout=1000)
 	time.sleep(0.2)
 
-
 def pen_up(amt=0):
 	motor_pen.run_to_rel_pos(position_sp=amt*PEN_DROP*-1, speed_sp=400, stop_action='hold')
 	motor_pen.wait_while('running',timeout=1000)
@@ -62,7 +62,6 @@ def pen_down(amt=0):
 	motor_pen.run_to_rel_pos(position_sp=amt*PEN_DROP, speed_sp=400, stop_action='hold')
 	motor_pen.wait_while('running',timeout=1000)
 	time.sleep(0.1)
-
 
 def go_to(pos=0):
 	start = str(motor_print_head.position)
@@ -86,9 +85,10 @@ def pen_draw(amt=0):
 
 
 
+# This is just a fun experiment to print my name "JOSH"
 def josh():
-	# LETTER J
 
+	# LETTER J
 	pen_down(3)
 	right(20)
 	pen_up(3)
@@ -146,30 +146,24 @@ def checkRemainingPixels( j, pixels ):
 	return True
 
 
-
-
-
-
-
-
 def reset_motors():
 	motor_linefeed.reset()
 	motor_print_head.reset()
 	motor_pen.reset()
 
 
+
 def main(filename='josh.png'):
 
 	reset_motors()
 
-	print (filename)
-
 	img = png.Reader(filename=filename)
 	imgFloat = img.asFloat()
-	print ( imgFloat )
+	print('* Parsed data from PNG file:')
+	print(imgFloat)
 	planes = imgFloat[3]['planes']
 
-
+	# Need to change these to be variables so it's clearer what I'm multiplying.
 	horiz_amt = 5*3
 	vert_amt = 2*8
 
@@ -189,7 +183,12 @@ def main(filename='josh.png'):
 				position -= horiz_amt
 
 		pen_rows.append(pen_positions)
+
+	print('* Calculated pen positions:')
 	print (pen_rows)
+
+	print('\n\n')
+	print('* BEGINNING TO PRINT')
 
 	cr()
 	forward(12)
@@ -204,6 +203,7 @@ def main(filename='josh.png'):
 
 	reset_motors()
 
+	print('* PRINTING COMPLETE!')
 
 
 if __name__ == "__main__":
@@ -211,10 +211,11 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-f", "--filename", help="PNG filename")
 	args = parser.parse_args()
-	print(args.filename)
 	if args.filename:
 		try:
+			print('RUNNING. Loading file `' + args.filename + '`')
 			main(args.filename)
 		except:
+			print('ERROR. There was a problem. Exiting.')
 			reset_motors()
 			exit()
